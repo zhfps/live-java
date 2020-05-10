be6b81d5596b7fd109cf0539ed674d70e486316f
it_dog <it_dog_zhang@163.com>
2020-05-10 13:32:06 +0800

    iniy

---
+++ src/main/java/com/live/zhf/LiveJavaApplication.java
@@ -0,0 +1,13 @@
+package com.live.zhf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiveJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LiveJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/live/zhf/LiveJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.live.zhf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiveJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
